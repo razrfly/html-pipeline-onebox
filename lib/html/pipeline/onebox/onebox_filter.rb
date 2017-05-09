69a6d5b4f44b69059fbc8724bbdc655184146508
Wojciech Druzkawiecki <exerq4@gmail.com>
2017-05-09 14:01:10 +0200

    Onebox filter

--- lib/html/pipeline/onebox/onebox_filter.rb
+++ lib/html/pipeline/onebox/onebox_filter.rb
@@ -5,7 +5,17 @@ module HTML
     # HTML Filter for Onebox.
     class OneboxFilter < TextFilter
       def call
+        regex = /(\s|\\r|\\t|\\f|\\n)+(https?:\/\/|www\.)\S*/
+        @text.gsub(regex) do |element|
+          url = element.strip
+          parsed_element = ::Onebox.preview(url).to_s.strip
 
+          if parsed_element.present?
+            element.gsub(url, parsed_element)
+          else
+            element
+          end
+        end
       end
     end
   end

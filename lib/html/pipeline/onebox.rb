260c83c8a2fc61681efaad3d04e2860a5266e3b8
Wojciech Druzkawiecki <exerq4@gmail.com>
2017-05-09 10:03:40 +0200

    Init commit

---
+++ lib/html/pipeline/onebox.rb
@@ -0,0 +1,9 @@
+require "html/pipeline/onebox/version"
+
+module Html
+  module Pipeline
+    module Onebox
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/html/pipeline/onebox/version.rb
@@ -0,0 +1,7 @@
+module Html
+  module Pipeline
+    module Onebox
+      VERSION = "0.1.0"
+    end
+  end
+end
